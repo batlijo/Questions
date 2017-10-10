44155662d733f7cb8c800c63e5486dcaec96b376
Batlijo <batlijoselevitz@gmail.com>
2017-10-10 11:49:10 -0700

    current working file

--- Javascript_Questions.js
+++ Javascript_Questions.js
@@ -70,4 +70,87 @@ function hoistedFunction () {
 var notHoistedFunction = function () {
 	console.log("I am not defined immediately.");
 } 
-^^^ WHYYYY IS THIS NOT DEFINED???????????? IS IT BECAUSE THERE'S A VAR?
+^^^ WHYYYY IS THIS NOT DEFINED???????????? IS IT BECAUSE THERE'S A VAR? ALSO 
+
+--
+IF STATEMENTS
+Example
+if ( answer === 42) {
+	console.log("Told you so!");
+}
+
+ELSE STATEMENTS
+If the condition is true, statement1 will be executed. Otherwise
+statement2 will be executed.
+if (condition) {
+	statement1: code that runs if condition is true
+} else {
+	statement2: code that runs if condition is false
+}
+Example
+if (gender == "male") {
+	console.log("hello, sir!");
+} else {
+	console.log("Hello, ma'am!");
+}
+
+ELSE IF STATEMENTS
+This is like an else statement, but with its own condition. It will only 
+if condition is true, and the previous statement's condition was false.
+SYNTAX
+else if . If the condition is true, statement1 will be excecuted. Otherwise, condition2 is checked .
+if it is true, then statement2 is executed. Else , if nothing is true , statement3
+is executed.
+if (condition1) {
+	statement1;
+} else if (condition2) {
+	statement2;
+} else {
+	statement3;
+}
+EXAMPLE
+
+if (someNumber > 10) {
+	console.log ("Numbers larger than 10 are not allowed.");
+} else if (someNumber < 0) {
+	console.log("Negative numbers are not allowed.");
+} else {
+	console.log("Nice number!");
+}
+--
+FOR LOOPS
+You use for loops, if you know how often you'll loop. The momst often used
+is varName in loops is i
+SYNTAX
+for ([var i= startValue]; [i < endValue]; [i+stepValue]) {
+	// Your code here
+}
+EXAMPLE
+for (var i = 0; i < 5; itt) {
+	console.log(i); //Prints the numbers from 0 to 4
+}
+EXAMPLE
+var i; // "outsourcing" the definition
+for (i=10; i >= 1; i--) {
+	console.log(i); //Prints the numbers from 10 to 1
+}
+EXAMPLE - Note that all of the three statements are optional, i.e.
+var i = 9;
+for(;;){
+	if(i === 0)break;
+	console.log(i);
+	i--;
+}
+--
+WHILE LOOPS
+SYNTAX
+while (condition) {
+	// Your code here
+}
+EXAMPLE
+var x = 0
+while (x < 5) {
+	console.log(x); // prints numbers from 0 to 4
+	x++;
+}
+
