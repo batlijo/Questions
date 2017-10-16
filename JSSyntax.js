80e06160389a16e358de0e89122e2585c1a48f7c
Batlijo <batlijoselevitz@gmail.com>
2017-10-16 12:28:47 -0700

    current working file

--- JSSyntax.js
+++ JSSyntax.js
@@ -1,4 +1,25 @@
-//Button Function
+// JQuery Button Function
 #$("#button").on("click", function ()){
 	console.log(button);
 });
+// JQuery object for button
+console.log(this);
+// });
+
+// JQuery HTML Button Function
+<button id="button">CLICK ME!</button>
+
+// Declaring a variable
+var website = "bwimpn" // bwimpn in variable output
+console.log(website); // Displays variable website's content to console
+
+Start on click function JQuery
+$("#start").on("click", function () {
+	// set the button' alert's timeout to run five seconds after the function's called.
+	delayButtonAlert = setTimeOut(function() {
+		alert("Alert #2");
+	}, 5000);
+});
+
+
+
